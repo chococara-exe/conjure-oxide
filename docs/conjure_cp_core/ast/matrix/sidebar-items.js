e3717a0f3b8ac931f7007f0b997b35bf775a77a9
chococara-exe <153284328+chococara-exe@users.noreply.github.com>
2025-11-18 18:22:26 +0000

    Actions: Rustdoc documentation for

--- docs/conjure_cp_core/ast/matrix/sidebar-items.js
+++ docs/conjure_cp_core/ast/matrix/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["enumerate_indices","flatten","flatten_enumerate","index_domains"]};
+window.SIDEBAR_ITEMS = {"fn":["enumerate_index_union_indices","enumerate_indices","flatten","flatten_enumerate","index_domains"]};
